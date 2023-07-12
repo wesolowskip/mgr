3637e0ea061866d02b17cce42fd4e7acaf817ad6
Paweł <wesolowski.p@protonmail.com>
2023-07-12 18:50:31 +0200

    fix

--- 2_merlin/submit_fit.sh
+++ 2_merlin/submit_fit.sh
@@ -2,5 +2,6 @@
 #SBATCH -C dgx
 #SBATCH --mem 60G
 #SBATCH --time 1-0:0:0
+#SBATCH --gpus 1
 
 singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "python -u fit_workflow.py --workflow-dir /scratch/shared/pwesolowski/mgr-pipeline/merlin --blocksize 1GiB --data-dir /scratch/shared/pwesolowski/mgr-pipeline/joined-recommender"
