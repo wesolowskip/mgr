eea09be05399c6674994c15eb2eb692747a077ce
Paweł <wesolowski.p@protonmail.com>
2023-07-18 21:59:18 +0200

    final 2_merlin

--- 2_merlin/merlin.Dockerfile
+++ 2_merlin/merlin.Dockerfile
@@ -1,10 +1,25 @@
 FROM cuml-prod
 
+RUN apt remove -y libopenmpi3 openmpi-common libopenmpi-dev openmpi-bin
+
+ENV OMPI_DIR=/opt/ompi
+
+RUN <<EOF cat > /root/tmp_bashrc && mv /root/tmp_bashrc /root/.bashrc
+`cat /root/.bashrc`
+export PATH=$OMPI_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$OMPI_DIR/lib:$LD_LIBRARY_PATH
+EOF
+
 SHELL ["/bin/bash", "-i", "-c"]
 RUN conda activate rapids
 RUN conda update -n base -c conda-forge conda
-
 RUN pip install --upgrade pip
+
+COPY pmix /usr/lib/x86_64-linux-gnu/pmix
+
+# RUN apt update && apt install libfabric-dev libpsm2-dev
+
+RUN mkdir -p /tmp/ompi && mkdir -p /opt && OMPI_VERSION=4.0.3 && OMPI_URL="https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-$OMPI_VERSION.tar.bz2" && cd /tmp/ompi && wget -O openmpi-$OMPI_VERSION.tar.bz2 $OMPI_URL && tar -xjf openmpi-$OMPI_VERSION.tar.bz2 && cd /tmp/ompi/openmpi-$OMPI_VERSION && ./configure --disable-mpi-fortran --build=x86_64-linux-gnu --prefix=$OMPI_DIR --sysconfdir=/etc --with-package-string='Debian OpenMPI' --enable-opal-btl-usnic-unit-tests --with-libevent=external --with-pmix=/usr/lib/x86_64-linux-gnu/pmix --enable-mpi-cxx --with-libltdl --with-devel-headers --with-slurm --with-sge --without-tm --sysconfdir=/etc/openmpi && make -j8 install
 
 RUN pip install nvidia-cudnn-cu11==8.6.0.163
 
@@ -17,5 +32,9 @@ RUN pip install tensorflow==2.12.*
 RUN conda install -y -c conda-forge -c nvidia merlin-core merlin-models merlin-systems nvtabular transformers4rec
 RUN pip install graphviz
 
-RUN pip install horovod
+RUN HOROVOD_GPU_OPERATIONS=NCCL pip install horovod
+
+
+# https://stackoverflow.com/questions/49130563/could-not-determine-size-of-character-while-installing-openmpi
+#./configure --disable-mpi-fortran --build=x86_64-linux-gnu --prefix=$OMPI_DIR --sysconfdir=/etc --with-package-string='Debian OpenMPI' --with-verbs --with-libfabric --with-psm2 --enable-opal-btl-usnic-unit-tests --with-libevent=external --with-pmix=/usr/lib/x86_64-linux-gnu/pmix --with-libltdl --with-slurm --with-sge --without-tm --sysconfdir=/etc/openmpi
 
---
+++ 2_merlin/submission.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --time 1-0:0:0
+#SBATCH -C dgx
+#SBATCH --gpus 1
+#SBATCH --mem 40G
+#SBATCH --cpus-per-gpu 8
+
+set -xu
+
+export WORKFLOW_DIR="/scratch/shared/pwesolowski/mgr-pipeline/merlin/"
+export DATA_DIR="/scratch/shared/pwesolowski/mgr-pipeline/joined-recommender"
+
+for BATCH_SIZE in 8192 16384 32768 65536;  do
+  for CUFILE_THREAD_COUNT in 4 8 16 32 64; do
+    for BLOCKSIZE in "512MiB" "1GiB" "2GiB" "4GiB"; do
+      for PARTS_PER_CHUNK in  1 2 3 4; do
+        singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "cd ~/praca-mgr/pipelines-repo/2_merlin && export PMIX_MCA_gds=^ds12 && ./hvd_wrapper.sh python -u run_merlin.py --workflow-dir ${WORKFLOW_DIR} --blocksize ${BLOCKSIZE}  ${FORCE_HOST_READ} --data-path ${DATA_DIR} --parts-per-chunk ${PARTS_PER_CHUNK} --force-host-read --batch-size ${BATCH_SIZE} --epochs 5"
+      done
+    done
+  done
+done
+
+for BATCH_SIZE in 8192 16384 32768 65536;  do
+  for CUFILE_THREAD_COUNT in 4 8 16 32 64; do
+    for BLOCKSIZE in "512MiB" "1GiB" "2GiB" "4GiB"; do
+      for PARTS_PER_CHUNK in  1 2 3 4; do
+        singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "cd ~/praca-mgr/pipelines-repo/2_merlin && export PMIX_MCA_gds=^ds12 && ./hvd_wrapper.sh python -u run_merlin.py --workflow-dir ${WORKFLOW_DIR} --blocksize ${BLOCKSIZE}  ${FORCE_HOST_READ} --data-path ${DATA_DIR} --parts-per-chunk ${PARTS_PER_CHUNK} --cufile-thread-count ${CUFILE_THREAD_COUNT} --batch-size ${BATCH_SIZE} --epochs 5"
+      done
+    done
+  done
+done
+
+
+#srun -l --time 6:0:0 -C dgx --nodes 1 --ntasks-per-node 2 --cpus-per-gpu 1 --cpu-bind=none --mpi=pmix --mem 32G --gpus-per-task 1 singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "cd ~/praca-mgr/pipelines-repo/2_merlin && export PMIX_MCA_gds=^ds12 && ./hvd_wrapper.sh python -u run_merlin.py --workflow-dir ${WORKFLOW_DIR} --blocksize 512MiB  --force-host-read --data-path ${DATA_DIR} --parts-per-chunk 1 --batch-size 16384 --epochs 5"
