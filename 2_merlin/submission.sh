8dd7da1deb76c3a731dca29fd4f10fc33493faab
Paweł <wesolowski.p@protonmail.com>
2023-07-18 23:38:21 +0200

    more time per job

--- 2_merlin/submission.sh
+++ 2_merlin/submission.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --time 1-0:0:0
+#SBATCH --time 10-0:0:0
+#SBATCH --partition long
 #SBATCH -C dgx
 #SBATCH --gpus 1
 #SBATCH --mem 40G
@@ -12,7 +13,7 @@ export DATA_DIR="/scratch/shared/pwesolowski/mgr-pipeline/joined-recommender"
 
 for BATCH_SIZE in 8192 16384 32768;  do
   for BLOCKSIZE in "512MiB" "1GiB" "2GiB"; do
-    for PARTS_PER_CHUNK in  1 2 3 4; do
+    for PARTS_PER_CHUNK in  1 2 3; do
       singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "cd ~/praca-mgr/pipelines-repo/2_merlin && export PMIX_MCA_gds=^ds12 && ./hvd_wrapper.sh python -u run_merlin.py --workflow-dir ${WORKFLOW_DIR} --blocksize ${BLOCKSIZE} --data-path ${DATA_DIR} --parts-per-chunk ${PARTS_PER_CHUNK} --force-host-read --batch-size ${BATCH_SIZE} --epochs 5"
     done
   done
@@ -21,7 +22,7 @@ done
 for BATCH_SIZE in 8192 16384 32768;  do
   for CUFILE_THREAD_COUNT in 4 8 16 32 64; do
     for BLOCKSIZE in "512MiB" "1GiB" "2GiB"; do
-      for PARTS_PER_CHUNK in  1 2 3 4; do
+      for PARTS_PER_CHUNK in  1 2 3; do
         singularity run --nv -B /scratch/shared/pwesolowski,/run/udev:/run/udev:ro ~/containers/merlin-prod.sif /bin/bash --rcfile ~/containers/singularity_rc -ci "cd ~/praca-mgr/pipelines-repo/2_merlin && export PMIX_MCA_gds=^ds12 && ./hvd_wrapper.sh python -u run_merlin.py --workflow-dir ${WORKFLOW_DIR} --blocksize ${BLOCKSIZE} --data-path ${DATA_DIR} --parts-per-chunk ${PARTS_PER_CHUNK} --cufile-thread-count ${CUFILE_THREAD_COUNT} --batch-size ${BATCH_SIZE} --epochs 5"
       done
     done
