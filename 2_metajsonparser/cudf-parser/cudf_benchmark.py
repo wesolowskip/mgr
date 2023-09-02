6b76f4871f06c5d405e42c224fd0465128fde3aa
Paweł <wesolowski.p@protonmail.com>
2023-09-02 21:12:44 +0200

    final changes

---
+++ 2_metajsonparser/cudf-parser/cudf_benchmark.py
@@ -0,0 +1,48 @@
+import gc
+import os
+import time
+import traceback
+from pathlib import Path
+
+from linetimer import CodeTimer
+
+import dask_cudf
+
+
+def benchmark_read_json(force_host_read, blocksize, cufile_params=None):
+    ddf = dask_cudf.read_json(
+        "/scratch/shared/pwesolowski/mgr-pipeline/joined-cuml/*.json", blocksize=blocksize,
+    )
+    for i in range(6):
+        with CodeTimer(f"{i=}, {force_host_read=}, {blocksize=}, {cufile_params=}"):
+            rows = ddf.shape[0].compute()
+        print(f"{rows=}")
+
+
+if __name__ == "__main__":
+
+    for blocksize in ["512MiB", "1GiB", "2GiB", "4GiB", "8GiB"]:
+        print("Working...")
+        os.environ["LIBCUDF_CUFILE_POLICY"] = "OFF"
+        benchmark_read_json(
+            force_host_read=True, blocksize=blocksize
+        )
+
+        os.environ["CUFILE_ENV_PATH_JSON"] = str(Path(__file__).parent.parent.resolve() / "cufile.json")
+        os.environ["LIBCUDF_CUFILE_POLICY"] = "GDS"
+
+        for cufile_thread_count in [4, 8, 16, 32, 64]:  # 64 for NY resulted in OOM
+            for cufile_slice_size_mb in [1]:
+
+                try:
+                    os.environ["LIBCUDF_CUFILE_THREAD_COUNT"] = str(cufile_thread_count)
+                    os.environ["LIBCUDF_CUFILE_SLICE_SIZE"] = str(cufile_slice_size_mb * 1024 * 1024)
+                    print("Working...")
+                    benchmark_read_json(
+                        force_host_read=False, blocksize=blocksize,
+                        cufile_params=f"{cufile_thread_count=}, {cufile_slice_size_mb=}"
+                    )
+                except Exception:
+                    traceback.print_exc()
+                gc.collect()
+                time.sleep(1)
