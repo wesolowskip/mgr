495170e7515c96db8c40aeb1be31ad924fc6ff6c
Paweł <wesolowski.p@protonmail.com>
2023-09-03 18:16:57 +0200

    Dask does not support GDS at the moment

--- 2_metajsonparser/cudf-parser/run_dask.sh
+++ 2_metajsonparser/cudf-parser/run_dask.sh
@@ -4,9 +4,10 @@ set -xu
 
 for i in {1..6}
 do
+  # Dask does not support GDS for JSON at the moment
   LIBCUDF_CUFILE_POLICY="OFF" python -u cudf_dask_benchmark.py
 
-  for LIBCUDF_CUFILE_THREAD_COUNT in 64 32 16 8 4; do
-    LIBCUDF_CUFILE_POLICY="GDS" LIBCUDF_CUFILE_SLICE_SIZE=1048576 LIBCUDF_CUFILE_THREAD_COUNT=${LIBCUDF_CUFILE_THREAD_COUNT} python -u cudf_dask_benchmark.py
-  done
+#  for LIBCUDF_CUFILE_THREAD_COUNT in 64 32 16 8 4; do
+#    LIBCUDF_CUFILE_POLICY="GDS" LIBCUDF_CUFILE_SLICE_SIZE=1048576 LIBCUDF_CUFILE_THREAD_COUNT=${LIBCUDF_CUFILE_THREAD_COUNT} python -u cudf_dask_benchmark.py
+#  done
 done
